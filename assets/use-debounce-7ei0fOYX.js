90fb21ceb07ef822853e2367fa5b1dbfbc0fe483
Perinban <Perinban@users.noreply.github.com>
2025-04-17 22:31:32 +0000

    deploy: d68d328e2a943525de32f8c2f4ec99b3680810a5

---
+++ assets/use-debounce-7ei0fOYX.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-BOzxTU4D.js";function c(e,t){const[r,u]=o.useState(e);return o.useEffect(()=>{const n=setTimeout(()=>{u(e)},t);return()=>{clearTimeout(n)}},[e,t]),r}export{c as u};
