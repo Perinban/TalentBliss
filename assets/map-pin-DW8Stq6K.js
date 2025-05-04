59c5bdcdbb3639484c1d40fd465da11bbcd4cfa2
Perinban <Perinban@users.noreply.github.com>
2025-05-04 09:58:25 +0000

    deploy: 094e2977cdfcf1b617dd4ec82e3d941ab87c7194

--- assets/map-pin-DW8Stq6K.js
+++ assets/map-pin-DW8Stq6K.js
@@ -1,4 +1,4 @@
-import{c}from"./createLucideIcon-BJVc4PKY.js";/**
+import{c}from"./createLucideIcon-Bod2zbFH.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
